2944f7b2131b14f0e9a327345d44502135f5ce43
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-09-24 13:14:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# loopback-connector-cos
+
+IBM Cloud Object Storage connector for LoopBack
+
+_TODO: Add more content per https://loopback.io/doc/en/contrib/readmes.html_
---
+++ index.js
@@ -0,0 +1,8 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: loopback-connector-cos
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+/* export the connector constructor and any other public API here */
